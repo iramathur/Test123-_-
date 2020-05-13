18401bb5598c6a34c58de4c9be2c59b02a6da77d
itom-qe <50704200+itom-qe@users.noreply.github.com>
2020-05-13 21:38:25 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -19,7 +19,7 @@ variable "tenantId" {
 
 variable "resource_group" {
   description = "The name of your Azure Resource Group."
-  default     = "Terraform-Azure-Beginners-4"
+  default     = "terraform-group-script"
 }
 
 variable "prefix" {
